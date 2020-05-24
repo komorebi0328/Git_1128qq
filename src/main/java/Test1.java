0ed8f7fc5e37ab60f9ea3d6006d28588bfbfb606
tom_pro <645414793@qq.com>
2020-05-24 17:22:21 +0800

    111

---
+++ src/main/java/Test1.java
@@ -0,0 +1,9 @@
+/**
+ * @author wgsForever
+ * @create 2020-05-24 17:13
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("nothing will");
+    }
+}
